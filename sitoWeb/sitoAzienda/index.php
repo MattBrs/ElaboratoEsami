e3c1a78051d3d128d33477a42543d0757970f932
matteo brusarosco <48391475+MattBrs@users.noreply.github.com>
2021-05-07 09:54:49 +0200

    Add files via upload

---
+++ sitoWeb/sitoAzienda/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Vetreria Giavenale</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <style>
+            .content{
+                width: 600px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Vetreria Giavenale</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="#">Prodotti</a></li>
+                    <li><a href="#">Servizi</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="registrationForm.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="loginForm.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="content">
+            <h1>Azienda</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequatur culpa deserunt dolorem, facere illum ipsa, iure laboriosam maiores molestias
+                necessitatibus nemo quas quos rem repellat repellendus repudiandae sit veniam vitae.
+            </p>
+
+            <h3>Chi siamo?</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Animi consectetur necessitatibus quidem quis? Dolore nulla pariatur quae.
+                Adipisci, asperiores beatae corporis earum explicabo in labore odit
+                perspiciatis repellendus tempora voluptate.
+            </p>
+        </div>
+    </body>
+</html>
