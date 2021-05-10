8ec686b69c4412d4098cfaa035564f6df2d0765e
MattBrs <matteo.brusarosc@gmail.com>
2021-05-10 21:43:17 +0200

    registration and login with sql query

--- sitoWeb/sitoAzienda/paginaPersonale.php
+++ sitoWeb/sitoAzienda/paginaPersonale.php
@@ -17,6 +17,7 @@
         <title>Area personale</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="styles/logRegForm.css">
     </head>
     <body>
 
@@ -35,8 +36,8 @@
                 </ul>
             </div>
         </nav>
-
-        <?php
+        <div class="content">
+            <?php
             $nomeUtente = $_SESSION['nomeUtente'];
             if($nomeUtente != ""){
                 $conn = new mysqli($servername, $username, $password, $db_name);        //connessione database
@@ -55,6 +56,8 @@
                 header("location: loginForm.php");
                 exit();
             }
-        ?>
+            ?>
+        </div>
+
     </body>
 </html>
